e1872ee551e5eb519f6b041c8168e60249fab237
Kevin-VR-99 <kevinvelazquezrios@gmail.com>
2026-02-19 17:51:14 -0600

    feat: desarrollo del inedx page con links de cv's para descarga

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistemon</title>
+</head>
+<body>
+    <h1>Binevenido al sistemon</h1>
+    <br>
+    <img src="./imgteam.png">
+    <br>
+    <h2>Scrum team</h2>
+    <ul>
+        <li>
+            <a href="./kevin.docx">Descargar CV de Kevin Rios</a>
+        </li>
+        <li>
+            <a href="./ailton.docx">Descargar CV de Ailton Guadalupe</a>
+        </li>
+        <li>
+            <a href="./angel.docx">Descargar CV de Angel Gabriel</a>
+        </li>
+        <li>
+            <a href="./aurelio.docx">Descargar CV de Aurelio Fabian</a>
+        </li>
+        <li>
+            <a href="./rigoberto.docx">Descargar CV de Francisco Rigoberto</a>
+        </li>
+    </ul>
+</body>
+</html>
